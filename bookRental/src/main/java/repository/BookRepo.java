f1583021c4ff14053da4c6003a08fe38337fb52c
Michał Gorczyński <m.gorczynski94@gmail.com>
2021-04-06 14:41:41 +0200

    Add repositories
    user and book repositories added

---
+++ bookRental/src/main/java/repository/BookRepo.java
@@ -0,0 +1,10 @@
+package repository;
+
+import book.Book;
+
+import java.util.List;
+
+public class BookRepo {
+    private List<Book> bookRepo;
+
+}
---
+++ bookRental/src/main/java/repository/UserRepo.java
@@ -0,0 +1,9 @@
+package repository;
+
+import user.User;
+
+import java.util.List;
+
+public class UserRepo {
+    private List<User> userRepo;
+}
